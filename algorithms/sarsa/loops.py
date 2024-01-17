28b80aecfc09d76aaed44413a1f7eaf2a8b12142
Irina M. S <irinamardhatillah@gmail.com>
2024-01-17 16:54:01 +0700

    Implement SARSA algorithm on MountainCar-v0

---
+++ algorithms/sarsa/loops.py
@@ -0,0 +1,47 @@
+import random
+
+from typing import Callable
+from tqdm import tqdm
+
+
+def run(env, agent, n_episodes, training, epsilon=None):
+    # For plotting metrics
+    rewards_per_episode = []
+    max_positions_per_episode = []
+
+    for i in tqdm(range(0, n_episodes)):
+        state = env.reset()[0]
+
+        rewards = 0
+        max_position = -99
+
+        if epsilon is not None:
+            if isinstance(epsilon, float):
+                epsilon_ = epsilon
+            if isinstance(epsilon, Callable):
+                epsilon_ = epsilon(i)
+
+        done = False
+        while not done:
+            if epsilon and random.uniform(0, 1) < epsilon_:
+                action = env.action_space.sample()
+            else:
+                action = agent.get_action(state)
+
+            next_state, reward, terminated, truncated, _ = env.step(action)
+            done = terminated or truncated
+
+            if training:
+                agent.update_parameters(state, action, reward, next_state)
+
+            rewards += reward
+
+            if next_state[0] > max_position:
+                max_position = next_state[0]
+
+            state = next_state
+
+        rewards_per_episode.append(rewards)
+        max_positions_per_episode.append(max_position)
+
+    return rewards_per_episode, max_positions_per_episode
---
+++ examples/mountain_car_v0/main.py
@@ -0,0 +1,56 @@
+import gymnasium as gym
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+from algorithms.sarsa.loops import run
+from algorithms.sarsa.agent import SarsaAgent
+
+
+# Tuned hyperparameters
+learning_rate = 0.1
+discount_factor = 0.9
+
+# Exploration-exploitation prob
+epsilon_train = 0.1
+
+# Number of episodes
+n_episodes_train = 10000
+n_episodes_test = 1000
+
+# Define the environment and the agent
+env = gym.make("MountainCar-v0")
+env._max_episode_steps = 1000
+agent = SarsaAgent(env, alpha=learning_rate, gamma=discount_factor)
+
+# Train the agent
+rewards_per_episode, max_positions_per_episode = run(
+    env,
+    agent,
+    n_episodes=n_episodes_train,
+    training=True,
+    epsilon=epsilon_train,
+)
+
+# Plot training progress
+fig, ax = plt.subplots(figsize=(12, 4))
+ax.set_title("Rewards per episode")
+pd.Series(rewards_per_episode).plot(kind="line")
+plt.show()
+
+fig, ax = plt.subplots(figsize=(12, 4))
+ax.set_title("Max positions per episode")
+pd.Series(max_positions_per_episode).plot(kind="line")
+plt.show()
+
+# Evaluate the agent
+rewards_per_episode, max_positions_per_episode = run(
+    env,
+    agent,
+    n_episodes=n_episodes_test,
+    training=False,
+)
+
+n_completed = sum([1 if pos > 0.5 else 0 for pos in max_positions_per_episode])
+
+print(f"{n_completed} success out of {n_episodes_test} attemps")
